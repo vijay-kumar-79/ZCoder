a9d80475987877262c9455109841f7744c768196
santhosh bhargav gunta <santhoshbhargav2006@gmail.com>
2025-05-29 15:31:07 +0530

    login,register, protectedroute done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 function Home() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // Redirect to the login page if the user is not authenticated
+    const jwtoken = localStorage.getItem('jwtoken')
+    if (jwtoken === null || jwtoken === undefined) {
+      navigate('/login')
+    }
+  })
+
   return (
-    <h1>Home!</h1>
+    <div>Home</div>
   )
 }
 
