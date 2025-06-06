dae3f09047dc3287b63e7722c909bfecb355fd9e
Vijay Kumar <gvijay2403@gmail.com>
2025-06-06 12:04:08 +0530

    fix minor bug

--- frontend/src/components/Problemcard.jsx
+++ frontend/src/components/Problemcard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./ProblemCard.css";
+import "./Problemcard.css";
 import { CiBookmark } from "react-icons/ci";
 import { FaBookmark } from "react-icons/fa";
 
